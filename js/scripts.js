2b125bc4a5356d36b282a7ae0998148944f854be
Olivia Hinton <hinton.olivia@gmail.com>
2016-06-21 09:45:39 -0700

    add function to update page scores, add hide and show pages features

--- js/scripts.js
+++ js/scripts.js
@@ -31,16 +31,38 @@ function updateScoreboard (name, focus, time, money, score) {
   $(".scoreboard-score").text(newPlayer.playerScore);
 }
 
+function updatePageScores (focus, time, money, pageScore) {
+  $(".page-focus").text(focus);
+  $(".page-time").text(time);
+  $(".page-money").text(money);
+  $(".page-score").text(pageScore);
+}
+
+var clearPageValues = function (){
+  $(".page-focus").val("");
+  $(".page-time").val("");
+  $(".page-money").val("");
+  $(".page-score").val("");
+}
+
+
+
+
 $(document).ready(function() {
 
+//landing page
   $("form#get-user-name").submit(function(event){
     event.preventDefault();
 
     var name = $("#new-user-name").val();
     newPlayer = new Player(name);
     updateScoreboard();
+
+    $(".landing-page").hide();
+    $(".page-1").show();
   });
 
+//page 1
   $("#button-1A").click(function(event){
     event.preventDefault();
     // collect adjustments to our variables
@@ -58,9 +80,85 @@ $(document).ready(function() {
     //display updated scores depending on choices
     updateScoreboard();
     //display new values for page 1-A player choices
-    $(".page-focus").text(focus);
-    $(".page-time").text(time);
-    $(".page-money").text(money);
-    $(".page-score").text(pageScore);
+    updatePageScores();
+
+    $(".page-1").hide();
+    $(".page-2").show();
+
+  });
+
+//page 2 (branching page)
+  $("#go-to-3A").click(function(event){
+    event.preventDefault();
+
+    clearPageValues();
+
+    $(".page-2").hide();
+    $(".page-3A").show();
+
+  });
+
+  $("#go-to-3B").click(function(event){
+    event.preventDefault();
+
+    clearPageValues();
+
+    $(".page-2").hide();
+    $(".page-3B").show();
+
+  });
+
+//page 3A
+  $("#button-3A").click(function(event){
+    event.preventDefault();
+
+    clearPageValues();
+    // collect adjustments to our variables
+    var focus = parseInt($("input:radio[name=focus]:checked").val());
+    var time = parseInt($("input:radio[name=time]:checked").val());
+    var money = parseInt($("input:radio[name=money]:checked").val());
+    var pageScore = focus + time + money;
+
+    newPlayer.calculateFocus(focus);
+    newPlayer.calculateTime(time);
+    newPlayer.calculateMoney(money);
+    newPlayer.calculateScore();
+
+    console.log(newPlayer);
+    //display updated scores depending on choices
+    updateScoreboard();
+    //display new values for page 1-A player choices
+    updatePageScores();
+
+    $(".page-3A").hide();
+    $(".page-4A").show();
+
+  });
+
+//page 3B
+  $("#button-3B").click(function(event){
+    event.preventDefault();
+
+    clearPageValues();
+    // collect adjustments to our variables
+    var focus = parseInt($("input:radio[name=focus]:checked").val());
+    var time = parseInt($("input:radio[name=time]:checked").val());
+    var money = parseInt($("input:radio[name=money]:checked").val());
+    var pageScore = focus + time + money;
+
+    newPlayer.calculateFocus(focus);
+    newPlayer.calculateTime(time);
+    newPlayer.calculateMoney(money);
+    newPlayer.calculateScore();
+
+    console.log(newPlayer);
+    //display updated scores depending on choices
+    updateScoreboard();
+    //display new values for page 3-B player choices
+    updatePageScores();
+
+    $(".page-3B").hide();
+    $(".page-4B").show();
+
   });
 });
